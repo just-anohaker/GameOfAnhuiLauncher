7fa290629574e1d1d6035a1b8f833055f18db5d0
just-anohaker <just.anohaker@gmail.com>
2019-07-30 10:11:22 +0800

    fixedme

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const Dapp = {
     // Id: "81961c799bf3f47652e7bab01d4e8437134374a7a114cfab1e89621bf644ca89",
-    Id: "",
+    Id: "5c611b50ac47e136ffc10926895fffec7c32f25d32c2692abb471e250caaebf0",
     protocol: "http",
     // hostname: "192.168.2.58",
     hostname: "47.111.160.173",
